feddb9894f7ca44009c17fbacb1174c15c4cb67e
rabeaifeanyi <rabea@eschenhagen.eu>
2024-08-08 12:20:08 +0200

    adds documentation

--- ui/plot.py
+++ ui/plot.py
@@ -1,13 +1,15 @@
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 
-
-DOTCOLOR = 'black'
-SHADOWCOLOR = 'orange'
+DOTCOLOR = 'blue'
 DOTSIZE = 4
 DOTALPHA = 1
+SHADOWCOLOR = 'orange'
 SHADOWALPHA = 0.8
 VIDEOALPHA = 0.75
+MICCOLOR = 'red'
+MICSIZE = 5
+MICALPHA = 1
 
 # Visual Range Estimation -> TODO make this configurable
 XMIN = -2.5
@@ -16,27 +18,77 @@ YMIN = -1.75
 YMAX = 1.75
 Z = 2.0
 
-
-def create_plot(frame_width, frame_height):
-    # Set up figure dimensions to match room coordinates
-    fig = figure(width=frame_width, height=frame_height, 
-                 x_range=(XMIN, XMAX), y_range=(YMIN, YMAX),
-                 output_backend='webgl')
+def create_plot(frame_width, frame_height, mic_positions):
+    """ Create a Bokeh plot with the given frame dimensions.
     
-    # Initialize ColumnDataSource for circles
-    cds = ColumnDataSource(data=dict(x=[], y=[], s=[]))
-    fig.scatter(x='x', y='y', legend_label='Strength of Source', marker='circle', size='s', 
-                color=SHADOWCOLOR, alpha=SHADOWALPHA, source=cds)
-    fig.scatter(x='x', y='y', legend_label='Sound Source', marker='circle', size=DOTSIZE, 
-                color=DOTCOLOR, alpha=DOTALPHA, source=cds)
+    Args:
+        frame_width (int): The width of the frame.
+        frame_height (int): The height of the frame.
+        mic_positions (np.ndarray): The positions of the microphones.
+    """
+    # Set up figure dimensions to match room coordinates
+    fig = figure(width=frame_width, 
+                 height=frame_height, 
+                 x_range=(XMIN, XMAX), 
+                 y_range=(YMIN, YMAX),
+                 output_backend='webgl')
     
     # Initialize ColumnDataSource for camera image
     cameraCDS = ColumnDataSource({'image_data': []})
-    fig.image_rgba(image='image_data', x=XMIN, y=YMIN, dw=(XMAX - XMIN), dh=(YMAX - YMIN), 
-                   source=cameraCDS, alpha=VIDEOALPHA)
     
-    return fig, cds, cameraCDS
+    fig.image_rgba(image='image_data', 
+                   x=XMIN, 
+                   y=YMIN, 
+                   dw=(XMAX-XMIN), 
+                   dh=(YMAX-YMIN), 
+                   source=cameraCDS, 
+                   alpha=VIDEOALPHA)
+    
+    # Initialize ColumnDataSource for circles
+    cds = ColumnDataSource(data=dict(x=[], y=[], s=[]))
+    fig.scatter(x='x', 
+                y='y', 
+                legend_label='Strength of Source', 
+                marker='circle', 
+                size='s', 
+                color=SHADOWCOLOR, 
+                alpha=SHADOWALPHA, 
+                source=cds)
+    
+    fig.scatter(x='x', 
+                y='y', 
+                legend_label='Sound Source', 
+                marker='circle', 
+                size=DOTSIZE, 
+                color=DOTCOLOR, 
+                alpha=DOTALPHA, 
+                source=cds)
+    
+    # Initialize ColumnDataSource for microphones
+    mic_cds = ColumnDataSource(data=dict(x=[], y=[]))
+    mic_cds.data = dict(x=mic_positions[0], y=mic_positions[1])
+    fig.scatter(x='x', 
+                y='y', 
+                legend_label='Microphones', 
+                marker='circle', 
+                size=MICSIZE, 
+                color=MICCOLOR, 
+                alpha=MICALPHA, 
+                source=mic_cds)
+    
+    # mic_positions:
+    # [[ 0.021  0.063  0.021  0.063  0.021  0.063  0.021  0.063 -0.063 -0.021 -0.063 -0.021 -0.063 -0.021 -0.063 -0.021]
+    #  [-0.063 -0.063 -0.021 -0.021  0.021  0.021  0.063  0.063  0.063  0.063  0.021  0.021 -0.021 -0.021 -0.063 -0.063]
+    #  [ 0.     0.     0.     0.     0.     0.     0.     0.     0.     0.     0.     0.     0.     0.     0.     0.   ]]
+    
+    return fig, cds, mic_cds, cameraCDS
+
 
 def update_plot(cds, model_data):
-    # Update the ColumnDataSource with data from the model
+    """Update the plot with the given data.
+
+    Args:
+        cds (_type_): _description_
+        model_data (_type_): _description_
+    """
     cds.data = dict(x=model_data['x'], y=model_data['y'], s=model_data['s'])
--- ui/video.py
+++ ui/video.py
@@ -3,13 +3,25 @@ import numpy as np
 
 
 class VideoStream:
+    """Class for reading video frames from a video capture object.
+    """
     def __init__(self, frame_width, frame_height, vc):
+        """Initialize the video stream with the given frame dimensions and video capture object.
+        
+        Args:
+            frame_width (int): The width of the frame.
+            frame_height (int): The height of the frame.
+            vc (cv2.VideoCapture): The video capture object.
+        """
         self.vc = vc
         self.frame_width = frame_width
         self.frame_height = frame_height
 
     def get_frame(self):
+        """Read a frame from the video capture object and return it as an RGBA image.
+        """
         rval, frame = self.vc.read()
+        
         if rval:
             # Resizing the frame to fit in the desired dimensions
             frame = cv2.resize(frame, (self.frame_width, self.frame_height))
@@ -21,6 +33,6 @@ class VideoStream:
             view[:, :, 2] = frame[:, :, 0]
             view[:, :, 1] = frame[:, :, 1]
             view[:, :, 3] = 255  # Set alpha channel to fully visible
-
             return img
+        
         return None
