f2178c9e3887848fc16746af2a8e986c9297fc2d
rabeaifeanyi <rabea@eschenhagen.eu>
2024-10-11 14:49:31 +0200

    adds documentation of problem

--- data_processing/process.py
+++ data_processing/process.py
@@ -15,6 +15,7 @@ from .sd_generator import SoundDeviceSamplesGeneratorWithPrecision
 # 1: Sample Splitter not correctly implemented #P1
 # 2: MaskedSpectraInOut not yet implemented #P2
 # 3: Beamforming not implemented #P3
+# 4: New model and Z-coordinate not yet tested #P4
 
 
 class Processor:
@@ -39,7 +40,7 @@ class Processor:
         self.results = {
             'x': [0],
             'y': [0],
-            #'z': [0],
+            'z': [0], #P4
             's': [0]
         }
 
@@ -202,7 +203,7 @@ class Processor:
                     self.results = {
                         'x': loc_pred[0].tolist(),
                         'y': loc_pred[1].tolist(),
-                        #'z': loc_pred[2].tolist(),
+                        'z': loc_pred[2].tolist(), #P4
                         's': strength_pred.tolist()
                     }
                 self._save_results()
